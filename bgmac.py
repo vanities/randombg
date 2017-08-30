3de7146d795e4d6956a2c2afaf303ef7b0af34e9
Adamant Mischke <mischkeaa@gmail.com>
2017-08-30 16:43:52 -0500

    got it working

--- bgmac.py
+++ bgmac.py
@@ -4,6 +4,8 @@ from random import randint
 from time import sleep
 from os.path import join, expanduser, isfile, exists
 from os import makedirs
+from subprocess import call
+from sys import stdout
 
 SCRIPT = """/usr/bin/osascript<<END 
 tell application "Finder" 
@@ -35,42 +37,59 @@ def get_wallpaper():
 	thread = randint(0,14)
 	post = 0
 
-	with urlopen('https://a.4cdn.org/wg/' + page + '.json') as url:
-		json = loads(url.read().decode())
+	try:
+		with urlopen('https://a.4cdn.org/wg/' + page + '.json') as url:
+			json = loads(url.read().decode())
 
-		thread = json['threads'][thread]
-		posts = thread['posts']
+			thread = json['threads'][thread]
+			posts = thread['posts']
 
-		for p in posts:
-			post += 1
+			for p in posts:
+				post += 1
 
-		post = randint(0,post-1)
-		
-		if 'ext' in posts[post]:
-			extension = str(posts[post]['ext'])
-			print('Found file from /wg/ @: ' + extension)
+			post = randint(0,post-1)
+			
+			if 'ext' in posts[post]:
+				extension = str(posts[post]['ext'])
 
-			if extension == '.jpg' or extension == '.png':
-				filename = str(posts[post]['tim'])
-				full_url = 'http://i.4cdn.org/wg/' + filename + extension
-				print(full_url)
-				file_path = get_wallpaper_path(filename + extension)
+				if extension == '.jpg' or extension == '.png':
+					filename = str(posts[post]['tim'])
+					full_url = 'http://i.4cdn.org/wg/' + filename + extension
+					print('Found file from /wg/ @: ' + full_url)
+					
+					file_path = get_wallpaper_path(filename + extension)
+					response = urlretrieve(full_url,file_path)
+					set_wallpaper(file_path)
+			else:
+				print('No file in this post..')
+				sleep(1)
+				main()
+	except (HTTPError):
+		print('Could not connect to /wsg/..')
+		sleep(1)
+		main()
 
-				response = urlretrieve(full_url,file_path)
-				set_wallpaper(file_path)
-		else:
-			print('No file in this post..')
-			sleep(1)
-			main()
+def script(code):
+	return call(
+		['bash', '-c', code],
+		shell=False,
+		stdin=None,
+		stdout=stdout,
+		stderr=stdout)
 
-'''
-ONLY SETS WALLPAPER FOR ONE DESKTOP AKA USELESS
-GOING TO USE THE BUILT IN SHIT FOR MAC TO CHANGE THE DESKTOP
+
 def set_wallpaper(file_path):
 	if isfile(file_path):
-		import subprocess
-		subprocess.Popen(SCRIPT%file_path, shell=True)
+	# See http://superuser.com/a/689804.
+		assert \
+			script('''
+			sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '%(file)s'"
+			killall Dock
+			''' % {
+			'file': file_path,
+			}) == 0, \
+			'Failed to set wallpaper'
 		print('Wallpaper set to ' + file_path)
-'''
+
 if __name__ == "__main__":
 	main()
