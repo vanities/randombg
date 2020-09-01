eb2cd6db9a483d1bce9f67d11f21eae83cdc67aa
Adamant Mischke <mischkeaa@gmail.com>
2020-08-31 21:46:00 -0500

    fix more stuff

--- random-wsg-bg.py
+++ random-wsg-bg.py
@@ -15,7 +15,8 @@ IMAGE_DIR = ''
 
 def main():
     # get that shit!
-    get_wallpaper()
+    wallpaper = get_wallpaper()
+    set_wallpaper(wallpaper)
 
 # assures the path for the file is good
 def get_wallpaper_path(file_name):
@@ -31,7 +32,8 @@ def get_wallpaper_path(file_name):
     return file_path
 
 # uses the 4chan api to get an image from /wg/
-def get_wallpaper():
+# returns the filepath
+def download_wallpaper():
 
     # random page & thread
     page = str(randint(0,9))
@@ -39,57 +41,44 @@ def get_wallpaper():
     post = 0
 
     # make sure we recieve the object from the api
-    try:
-        with urlopen('https://a.4cdn.org/wg/' + page + '.json') as url:
-            json = loads(url.read().decode())
+    attempts = 0
+    max_attempts = 10
+    while attempts < max_attempts:
+        try:
+            with urlopen('https://a.4cdn.org/wg/' + page + '.json') as url:
+                json = loads(url.read().decode())
 
-            # set the thread and post
-            thread = json['threads'][thread]
-            posts = thread['posts']
+                # set the thread and post
+                thread = json['threads'][thread]
+                posts = thread['posts']
 
-            # count the # of posts in thread
-            for p in posts:
-                post += 1
+                # random number post
+                post = randint(0, len(post)-1)
 
-            # random number post
-            post = randint(0,post-1)
+                # makes sure the post we have has an image
+                if 'ext' in posts[post]:
+                    extension = str(posts[post]['ext'])
 
-            # makes sure the post we have has an image
-            if 'ext' in posts[post]:
-                extension = str(posts[post]['ext'])
+                    # this better not be a gif or webm..
+                    if extension in ['.jpg', '.png', 'jpeg']:
 
-                # this better not be a gif or webm..
-                if extension in ['.jpg', '.png', 'jpeg']:
+                        # TODO implement another filter to filter nsfw
+                        filename = str(posts[post]['tim'])
+                        full_url = 'http://i.4cdn.org/wg/' + filename + extension
+                        print('Found file from /wg/ @: ' + full_url)
 
-                    # TODO implement another filter to filter nsfw
-                    filename = str(posts[post]['tim'])
-                    full_url = 'http://i.4cdn.org/wg/' + filename + extension
-                    print('Found file from /wg/ @: ' + full_url)
+                        # output file path
+                        filepath = get_wallpaper_path(filename + extension)
 
-                    # output file path
-                    file_path = get_wallpaper_path(filename + extension)
+                        # put the image in the directory specified
+                        response = urlretrieve(full_url,filepath)
 
-                    # put the image in the directory specified
-                    response = urlretrieve(full_url,file_path)
+                        return filepath
 
-            else:
-                print('No file in this post..')
-                sleep(1)    # api rule
-                main()
-    # why does 4chan time me out?
-    except Exception as e:
-        print('Could not connect to /wsg/..', e)
-        sleep(1)     # api rule
-        main()
-
-# script for setting the wallpaper
-def script(code):
-    return call(
-        ['bash', '-c', code],
-        shell=False,
-        stdin=None,
-        stdout=stdout,
-        stderr=stdout)
+        except Exception as e:
+            attempts += 1
+            print("Couldn't get wallpaper, e={} attempts={}".format(e,attempts))
+            sleep(1)
 
 
 # sets the wallpaper using sqlite3 db
@@ -101,8 +90,8 @@ def set_wallpaper(filepath):
             app('Finder').desktop_picture.set(mactypes.File(filepath))
             print('Wallpaper set to ' + filepath)
     elif platform == "linux":
-        if isfile(file_path):
-                subprocess.call(['feh', '--bg-fill', file_path],
+        if isfile(filepath):
+                subprocess.call(['feh', '--bg-fill', filepath],
                                 env=environ.copy())
 
 if __name__ == "__main__":
